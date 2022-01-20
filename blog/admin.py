653ae840d4461395a8d1f6ee2b24d0701c0784df
origin <ljhyun715@gmail.com>
2022-01-20 16:50:27 +0900

    commit(01/20)

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'mod_date')
+    list_filter = ('mod_date',)
+    search_fields = ('title', 'content')
